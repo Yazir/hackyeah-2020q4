021d62e23fd77c61a4b22c9b2af985c8dea86913
Atyxon <75041222+Atyxon@users.noreply.github.com>
2020-11-27 23:48:07 +0000

    yu

---
+++ Assets/Scripts/StreetGen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetGen : MonoBehaviour
+{
+    public GameObject empty;
+    public int segmentsAmount;
+    public GameObject[] segmentPrefabs;
+    public bool reverse;
+    int next;
+    private void Start()
+    {
+        for (int i = 0; i < segmentsAmount; i++)
+        {
+            int rs = Random.Range(0, segmentPrefabs.Length);
+            GameObject bob = Instantiate(segmentPrefabs[rs], empty.transform.localPosition + new Vector3(0,0,next), transform.rotation);
+            bob.transform.parent = empty.transform;
+            bob.transform.localPosition = new Vector3(0,0, next);
+            if (!reverse)
+                next += 12;
+            else
+                next -= 12;
+        }
+    }
+}
