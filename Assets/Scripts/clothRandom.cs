c62b1e51e2daf84e284971e6283f9bfd1be17dee
Atyxon <75041222+Atyxon@users.noreply.github.com>
2020-11-28 17:23:54 +0000

    random clothes

---
+++ Assets/Scripts/clothRandom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clothRandom : MonoBehaviour
+{
+    public GameObject[] up;
+    public GameObject[] down;
+    public GameObject[] hair;
+    public GameObject[] foot;
+    private void Start()
+    {
+        int upr = Random.Range(0, up.Length);
+        up[upr].SetActive(true);
+        int downr = Random.Range(0, down.Length);
+        down[downr].SetActive(true);
+        int hairr = Random.Range(0, hair.Length);
+        hair[hairr].SetActive(true);
+        int footr = Random.Range(0, foot.Length);
+        foot[footr].SetActive(true);
+    }
+}
