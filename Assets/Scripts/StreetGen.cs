54f4377ceeba070be9b58cd1b029529b0eb81617
Yazaa <rgkonduty@gmail.com>
2020-11-28 16:47:09 +0100

    gen fix

--- Assets/Scripts/StreetGen.cs
+++ Assets/Scripts/StreetGen.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class StreetGen : MonoBehaviour
 {
     public GameObject empty;
@@ -9,8 +11,14 @@ public class StreetGen : MonoBehaviour
     public GameObject[] segmentPrefabs;
     public bool reverse;
     int next;
+    
     private void Start()
     {
+        Generate();
+    }
+
+    private void Generate() {
+        Cleanup();
         for (int i = 0; i < segmentsAmount; i++)
         {
             int rs = Random.Range(0, segmentPrefabs.Length);
@@ -18,9 +26,15 @@ public class StreetGen : MonoBehaviour
             bob.transform.parent = empty.transform;
             bob.transform.localPosition = new Vector3(0,0, next);
             if (!reverse)
-                next += 12;
+                next += 14;
             else
-                next -= 12;
+                next -= 14;
+        }
+    }
+
+    private void Cleanup() {
+        foreach (Transform child in empty.transform) {
+            Destroy(child);
         }
     }
 }
