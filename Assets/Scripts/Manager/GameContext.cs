34ce681bbdf75f411b4fe69c2a3e3d88d9beaf11
Yazaa <rgkonduty@gmail.com>
2020-11-29 07:59:31 +0100

    flex tape coding

--- Assets/Scripts/Manager/GameContext.cs
+++ Assets/Scripts/Manager/GameContext.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 interface IGameContext
 {
@@ -13,6 +14,7 @@ public class GameContext : MonoBehaviour, IGameContext
     [SerializeField] private PlayerController playerController;
     [SerializeField] private MapManager mapManager;
     [SerializeField] private CameraController cameraController;
+    [SerializeField] private Image fadeImage;
 
     public PlayerController PlayerController => playerController;
     public MapManager MapManager => mapManager;
@@ -20,8 +22,55 @@ public class GameContext : MonoBehaviour, IGameContext
 
     public static GameContext instance;
     
+    private bool fadingOut = false;
+    private float timeElapsed;
+    private bool fadingIn;
+    
     private void Awake()
     {
         GameContext.instance = this;
+        FadeIn();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && Input.GetKey(KeyCode.B)) {
+            Time.timeScale += 0.5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.O) && Input.GetKey(KeyCode.B)) {
+            Time.timeScale = 1;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.I) && Input.GetKey(KeyCode.B)) {
+            Time.timeScale -= 0.1f;
+        }
+
+        timeElapsed += Time.deltaTime;
+
+        if (fadingOut) {
+            var color = fadeImage.color;
+            color.a += 0.25f * Time.deltaTime;
+            fadeImage.color = color;
+            if (color.a >= 1 - Mathf.Epsilon) fadingOut = false;
+        }
+
+        if (fadingIn) {
+            var color = fadeImage.color;
+            color.a -= 0.25f * Time.deltaTime;
+            fadeImage.color = color;
+            if (color.a <= Mathf.Epsilon) fadingIn = false;
+        }
+    }
+
+    public void FadeOut() {
+        fadingOut = true;
+        fadingIn = false;
+    }
+
+    public void FadeIn() {
+        fadingOut = false;
+        fadingIn = true;
     }
 }
